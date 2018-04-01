947ae7a3af6c4712d6d74c3e4411950b5e91822d
root <asbilyk@gmail.com>
2018-04-01 12:06:53 +0000

    changes

--- task4_1.sh
+++ task4_1.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-yum install dmidecode
+sudo apt-get install dmidecode 1>/dev/null
 exec 1>./task4_1.out
 echo '--- HARDWARE ---'
-echo "CPU: dmidecode --type processor"
-echo "RAM: dmidecode -t memory | grep -i size"
-echo "Motherboard:' dmidecode --type baseboard-manufacturer" 
-echo "System Serial Number: dmidecode -s system-serial-number"
+echo "CPU:"`dmidecode -t processor|grep Version|awk '{print $2 $3}'`
+echo "RAM:"`dmidecode -t memory|grep Size:|awk '{print $2+$4 $3}'`
+echo "Motherboard:"`dmidecode -t baseboard|grep Manufacturer` 
+echo "System Serial Number:"`dmidecode -t baseboard|grep Serial|awk '{print $3}'`
 echo '---System---'
-
+echo "OS Distribution:"`cat /etc/*-release|grep Description`
+echo "Kernel version"`uname -r`
+echo "Installation date:"`uname -a|awk -F SMP '{print $1}'`
+echo "Hostname:"`hostname -f`
+echo "Uptime:"`uptime|awk '{print $3}'`
+echo "Processes running:"`ps aux|wc -l`
+echo "Users logged in:"`w|awk -F, '{print $2}'|grep '[1-9]'`
 echo '---Network---'
-ip addr | grep inet[^6][\ 1-9.\/]
+ip addr |grep -i inet[^6]
