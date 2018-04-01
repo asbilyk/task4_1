0f925735a88cd9dc696b0959552a0e6342bbd752
alex bilyk <asbilyk@gmail.com>
2018-04-01 23:19:26 +0300

    fix for first task

--- task4_1.sh
+++ task4_1.sh
@@ -7,9 +7,9 @@ echo "RAM:"`dmidecode -t memory|grep Size:|awk '{print $2+$4 $3}'`
 echo "Motherboard:"`dmidecode -t baseboard|grep Manufacturer` 
 echo "System Serial Number:"`dmidecode -t baseboard|grep Serial|awk '{print $3}'`
 echo '---System---'
-echo "OS Distribution:"`cat /etc/*-release|grep Description`
+echo "OS Distribution:"`cat /etc/*-release|grep -i description`
 echo "Kernel version"`uname -r`
-echo "Installation date:"`uname -a|awk -F SMP '{print $1}'`
+echo "Installation date:"`uuname -a|awk '{print $6" "$7" "$8" "$9" "$10" "$11}'name -a|awk -F SMP '{print $1}'`
 echo "Hostname:"`hostname -f`
 echo "Uptime:"`uptime|awk '{print $3}'`
 echo "Processes running:"`ps aux|wc -l`
